598115a2cc45dd617bebe2a78c7dcccd068f99a3
Robert Arlt Jr <dukrat@gmail.com>
2017-09-17 19:06:30 -0400

    wma scan support v1

--- scan.py
+++ scan.py
@@ -52,7 +52,7 @@ important_extensions = [
     'tta',
     'ogg',
     #'flv', # not yet,
-    #'wma'
+    'wma'
     ]
 
 directories_to_ignore = [
@@ -234,6 +234,8 @@ def parse_song_info(db, filename):
         elif extlc in ['wav']:
             # gonna have to extract from the filename
             print 'skipping wav file for now %s' % filename.encode('utf-8')
+        elif extlc in ['wma']:
+            tags = mutagen.id3.ID3(filename)
         elif extlc in important_extensions:
             tags = mutagen.File(filename)
         # uncomment this if you want to check every file regardless of extension
